364a9a276bc9ceb25565272476c0a762ace936b5
Ryan Ho <ryanhohoho6@gmail.com>
2023-05-26 21:57:16 -0400

    trying to print without extra tools

--- webScraper.py
+++ webScraper.py
@@ -2,12 +2,11 @@
 #https://www.geeksforgeeks.org/implementing-web-scraping-python-beautiful-soup/
 
 import requests
-from bs4 import BeautifulSoup
 
 URL = "https://www.google.ca/"
 pageRequest = requests.get(URL)
 
-#print(pageRequest.content)
+print(pageRequest.content)
 
-soup = BeautifulSoup(pageRequest.content, 'html5lib')
-print(soup.prettify())
+#for word in range(10):
+    #print(str(pageRequest.content[word]))
