7cb2b78be179a33503ce831cd6d714dfe4752c0d
Ryan Ho <ryanhohoho6@gmail.com>
2023-05-27 21:23:30 -0400

    not understanding but still trying

--- webScraper.py
+++ webScraper.py
@@ -1,12 +1,31 @@
 #Learning directly from the one and only geeksforgeeks
 #https://www.geeksforgeeks.org/implementing-web-scraping-python-beautiful-soup/
 
-import requests
+#Installed External Libraries
+#pip install requests
+#pip install bs4
+#pip install html5lib
 
-URL = "https://www.google.ca/"
+import requests
+from bs4 import BeautifulSoup
+
+URL = "https://cardfight.fandom.com/wiki/Booster_Set_1:_Descent_of_the_King_of_Knights"
 pageRequest = requests.get(URL)
 
-print(pageRequest.content)
+#print(pageRequest.content)
 
-#for word in range(10):
-    #print(str(pageRequest.content[word]))
+parsedContent = BeautifulSoup(pageRequest.content, 'html5lib')
+#print(parsedContent.prettify())
+
+#for link in parsedContent.find_all('a'):
+    #print(link.get('href'))
+
+for row in parsedContent.find_all('td'):
+    link = row.find('a')
+
+    try:
+        #if 'href' in link.attrs:
+        print(link.get('href'))
+    except:
+        pass
+    #print(link)
